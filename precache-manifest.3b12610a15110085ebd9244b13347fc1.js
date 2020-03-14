deaa9a2fb04bce177edf101ace377a88999a0a2e
Mostafa Raafat <mostafaraafat@Mostafas-MacBook-Pro.local>
2020-03-15 00:08:37 +0200

    Updates

---
+++ precache-manifest.3b12610a15110085ebd9244b13347fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bab2754780344ce06a7eb890be97a68",
+    "url": "/fibonacci-game/index.html"
+  },
+  {
+    "revision": "1079dfa3b04a32f28df6",
+    "url": "/fibonacci-game/static/css/main.1071fd2e.chunk.css"
+  },
+  {
+    "revision": "93365fe620fa3e781359",
+    "url": "/fibonacci-game/static/js/2.fb79ce62.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/fibonacci-game/static/js/2.fb79ce62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1079dfa3b04a32f28df6",
+    "url": "/fibonacci-game/static/js/main.3011825d.chunk.js"
+  },
+  {
+    "revision": "975b42580da6a425dc19",
+    "url": "/fibonacci-game/static/js/runtime-main.946ac26d.js"
+  }
+]);
